5540b4376a6e0c5e5080fd044cdb56d42a6f27b4
adityatiwarig <aditya2461tiwari@gmail.com>
2025-05-31 23:33:31 +0530

    new project

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -40,7 +40,8 @@ function InputBox({
           className="w-full px-3 py-2 rounded-lg border border-gray-300 bg-gray-100 text-gray-800 cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
         >
           {currencyOptions.map((currency) => (
-            <option key={currency} value={currency}>
+            <option key={currency} value={currency}> 
+                                                       {/* key ago unique id debela lagaiha  */}
               {currency.toUpperCase()}
             </option>
           ))}
